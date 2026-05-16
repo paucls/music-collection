e69d3bc374faab8d26810ae78143f0d51d86ccee
Paulo Clavijo Esteban <paucls@gmail.com>
2026-05-16 21:48:57 +0100

    Try fixing path again

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
+    provideHttpClient()
   ]
 };
