7e819f3a83579780750774af0ba7c4bb16599bf4
Paulo Clavijo Esteban <paucls@gmail.com>
2026-05-16 21:21:25 +0100

    Filter by artist. Sort by artis and album title

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,8 +12,11 @@ import { AlbumComponent } from './album.component';
 })
 export class App implements OnInit {
   albums: Album[] = [];
+  filteredAlbums: Album[] = [];
   loading = true;
   error: string | null = null;
+  selectedArtist: string = '';
+  artists: string[] = [];
 
   constructor(private albumService: AlbumService, private cdr: ChangeDetectorRef) {}
 
@@ -24,6 +27,9 @@ export class App implements OnInit {
         console.log('Albums array:', data.albums);
         console.log('Albums count:', data.albums.length);
         this.albums = data.albums;
+        this.sortAlbums();
+        this.extractArtists();
+        this.filteredAlbums = [...this.albums];
         console.log('After assignment - this.albums:', this.albums);
         console.log('After assignment - this.albums.length:', this.albums.length);
         this.loading = false;
@@ -35,6 +41,28 @@ export class App implements OnInit {
         console.error(err);
       }
     });
+  }
+
+  sortAlbums() {
+    this.albums.sort((a, b) => {
+      const artistCompare = a.artist.localeCompare(b.artist);
+      if (artistCompare !== 0) return artistCompare;
+      return a.title.localeCompare(b.title);
+    });
+  }
+
+  extractArtists() {
+    const uniqueArtists = new Set(this.albums.map(album => album.artist));
+    this.artists = Array.from(uniqueArtists).sort();
+  }
+
+  filterByArtist(artist: string) {
+    this.selectedArtist = artist;
+    if (artist === '') {
+      this.filteredAlbums = [...this.albums];
+    } else {
+      this.filteredAlbums = this.albums.filter(album => album.artist === artist);
+    }
   }
 
   trackById(index: number, album: Album): string {
