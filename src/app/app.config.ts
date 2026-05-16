da0026e1841548d73672c3d69ef28aee30142867
Paulo Clavijo Esteban <paucls@gmail.com>
2026-05-16 21:42:23 +0100

    Fix routing to work with github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
